e16baa116d0e80e0f16c5f16a62bedf5e2419e2b
IDStack <idstack@outlook.co.id>
2021-03-23 23:29:45 +0700

    installation bootstrap jquery popper.js fontawesome

--- src/main.js
+++ src/main.js
@@ -2,4 +2,16 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+/* Bootstrap */
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css'
+
+/* jQuery */
+import jQuery from 'jquery'
+window.$ = jQuery
+
+/* Fontawesome */
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
+
 createApp(App).use(router).mount('#app')
