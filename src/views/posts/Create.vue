f3f2546c94f517a51d7718fae3f25c205f4ef6d2
IDStack <idstack@outlook.co.id>
2021-03-24 00:23:43 +0700

    styling create post page

--- src/views/posts/Create.vue
+++ src/views/posts/Create.vue
@@ -1,66 +1,113 @@
 <template>
-  <div>
-    <form @submit.prevent="handleSubmit">
-      <div>
-        <label for="">Title</label>
-        <input type="text" v-model="title" />
+  <!-- Page Header -->
+  <header class="masthead" style="background-image: url('img/contact-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+          <div class="page-heading">
+            <h1>Create new post</h1>
+          </div>
+        </div>
       </div>
-      <div>
-        <label for="">Body</label>
-        <textarea cols="30" rows="10" v-model="body"></textarea>
-      </div>
-      <div>
-        <label for="">Tags</label>
-        <input type="text" v-model="tag" @keydown.enter.prevent="handleKeydown"/>
-      </div>
-      <button type="submit">Create</button>
-    </form>
-    <div v-for="tag in tags" :key="tag">
-        #{{ tag }}
     </div>
-    {{ title }}
-    {{ body }}
+  </header>
+
+  <!-- Main Content -->
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-10 mx-auto">
+        <form @submit.prevent="handleSubmit">
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+              <label>Title</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Title Post"
+                required
+                v-model="title"
+              />
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+              <label>Body</label>
+              <textarea
+                rows="5"
+                class="form-control"
+                placeholder="Input body post"
+                required
+                v-model="body"
+              ></textarea>
+              <p class="help-block text-danger"></p>
+            </div>
+          </div>
+          <div class="control-group">
+            <div class="form-group floating-label-form-group controls">
+              <label>Tags</label>
+              <input
+                type="text"
+                class="form-control"
+                placeholder="Input Tags"
+                v-model="tag"
+                @keydown.enter.prevent="handleKeydown"
+              />
+              <p class="help-block text-danger"></p>
+            </div>
+            <span v-for="tag in tags" :key="tag">#{{ tag }}</span>
+          </div>
+          <br />
+          <button type="submit" class="btn btn-primary">
+            Send
+          </button>
+        </form>
+      </div>
+    </div>
   </div>
+
+  <hr />
 </template>
 
 <script>
 import { ref } from "@vue/reactivity";
-import { useRouter } from 'vue-router'
+import { useRouter } from "vue-router";
 
 export default {
   setup() {
-    const router = useRouter()
+    const router = useRouter();
     const title = ref("");
     const body = ref("");
     const tag = ref("");
     const tags = ref([]);
 
     const handleKeydown = () => {
-        if (! tags.value.includes(tag.value)) {
-            tag.value = tag.value.replace(/\s/g, '')
+      if (!tags.value.includes(tag.value)) {
+        tag.value = tag.value.replace(/\s/g, "");
 
-            tags.value.push(tag.value)
-        }
-        tag.value = ''
-    }
+        tags.value.push(tag.value);
+      }
+      tag.value = "";
+    };
 
     const handleSubmit = async () => {
-        const post = {
-            title: title.value,
-            body: body.value,
-            tags: tags.value
-        }
+      const post = {
+        title: title.value,
+        body: body.value,
+        tags: tags.value,
+      };
 
-        await fetch('http://localhost:3000/posts/', {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify(post)
-        })
+      await fetch("http://localhost:3000/posts/", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(post),
+      });
 
-        router.push({
-            name: 'Home'
-        })
-    }
+      router.push({
+        name: "Home",
+      });
+    };
 
     return { title, body, tag, tags, handleKeydown, handleSubmit };
   },
