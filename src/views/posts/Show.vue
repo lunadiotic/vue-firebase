b21cd229a5720e2878ca79d7b02a063b2a721c1b
IDStack <idstack@outlook.co.id>
2021-03-24 00:12:04 +0700

    styling detail post with bootstrap

--- src/views/posts/Show.vue
+++ src/views/posts/Show.vue
@@ -1,36 +1,60 @@
 <template>
- <div v-if="error">{{ error }}</div>
+  <div v-if="error">{{ error }}</div>
   <div v-if="post">
-      <h3>{{ post.title }}</h3>
-      <p>{{ post.body }}</p>
-      <span v-for="tag in post.tags" :key="tag">
-        #{{ tag }}
-      </span>
+    <!-- Page Header -->
+    <header class="masthead" style="background-image: url('img/post-bg.jpg')">
+        <div class="overlay"></div>
+        <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="post-heading">
+                <h1>{{ post.title }}</h1>
+                <span class="meta-post" v-for="tag in post.tags" :key="tag"> #{{ tag }} </span>
+            </div>
+            </div>
+        </div>
+        </div>
+    </header>
+
+    <!-- Post Content -->
+    <article>
+        <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+            <p>
+                {{ post.body }}
+            </p>
+            </div>
+        </div>
+        </div>
+    </article>
+
+    <hr />
   </div>
   <div v-else>
-      <Loading />
+    <Loading />
   </div>
+
 </template>
 
 <script>
-import getPost from '@/composable/getPost'
-import Loading from '@/components/Loading'
+import getPost from "@/composable/getPost";
+import Loading from "@/components/Loading";
 
 export default {
-    props: ['id'],
-    components: {
-        Loading
-    },
-    setup(props) {
-        const { post, error, load } = getPost(props.id)
+  props: ["id"],
+  components: {
+    Loading,
+  },
+  setup(props) {
+    const { post, error, load } = getPost(props.id);
 
-        load()
+    load();
 
-        return { post, error}
-    }
-}
+    return { post, error };
+  },
+};
 </script>
 
 <style>
-
 </style>
