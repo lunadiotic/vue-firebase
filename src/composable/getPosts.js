bce932ac3c4e564bde6ff4a2f50b43dc67fd6c03
IDStack <idstack@outlook.co.id>
2021-03-25 18:22:37 +0700

    fetch data post from firebase

--- src/composable/getPosts.js
+++ src/composable/getPosts.js
@@ -1,4 +1,5 @@
 import { ref } from 'vue'
+import { projectFirestore } from '@/firebase/config'
 
 const getPosts = () => {
     const posts = ref([])
@@ -6,14 +7,15 @@ const getPosts = () => {
 
     const load = async () => {
         try {
-            await new Promise(resolve => {
-                setTimeout(resolve, 2000)
+            const res = await projectFirestore.collection('posts').get()
+
+            posts.value = res.docs.map(doc => {
+                return {
+                    ...doc.data(), id: doc.id
+                }
             })
-            let data = await fetch('http://localhost:3000/posts')
-            if (! data.ok) {
-                throw Error('Tidak ada data')
-            }
-            posts.value = await data.json()
+            
+            console.log(res.docs)
         } catch (err) {
         error.value = err.message
         }
